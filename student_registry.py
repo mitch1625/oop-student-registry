d6d613222b391d33104ec34dad10753a9cf05a38
eric <ericmitchell1625@hotmail.com>
2023-10-10 18:07:49 -0400

    python

--- student_registry.py
+++ student_registry.py
@@ -1,2 +1,52 @@
 class Student:
-    pass
+    def __init__(self, name:str, age=13, grade="12th"):
+        self._name = name
+        self._age = age
+        self._grade = grade
+
+    def __str__(self):
+        return f'Student 1: Name: {self._name}, Age: {self._age}, Grade: {self._grade}'        
+    
+    @property
+    def get_name(self):
+        return self._name
+    
+    @property 
+    def get_age(self):
+        return self._age
+    
+    @property
+    def get_grade(self):
+        return self._grade
+    
+    @get_name.setter
+    def set_name(self, new_name):
+        if isinstance(new_name, str) and len(new_name) >= 3 and new_name.isalpha():
+            self._name = new_name
+        else:
+            print('Invalid Name')
+
+    @get_age.setter
+    def set_age(self, new_age):
+        if isinstance(new_age, int) and new_age > 11 and new_age < 18:
+            self._age = new_age
+
+    @get_grade.setter
+    def set_grade(self, new_grade):
+        if isinstance(new_grade, str) and new_grade == "9th" or new_grade == "10th" or new_grade == "11th" or new_grade == "12th":
+            self._grade = new_grade
+
+    def study(self, subject):
+        print(f'{self._name} is studying {subject}')
+
+    def advance(self): 
+        grade = self._grade
+        grade = str(int(grade[:-2])+1) + "th"
+        print(f'{self._name} has advanced to {grade} grade')
+
+
+
+list = [1,2,3,4,5]
+for elem in list:
+    if (elem[i] > 0):
+        print(elem)
--- test_student_registry.py
+++ test_student_registry.py
@@ -2,39 +2,40 @@ import pytest
 from student_registry import Student
 
 def test_student_initialization():
-    student = Student("Alice")
+    student = Student("Alice", 13, "12th")
     assert student.get_name == "Alice"
     assert student.get_age == 13
     assert student.get_grade == "12th"
 
 def test_student_name_setter():
-    student = Student("Alice")
+    student = Student("Alice", 12, "11th")
     student.set_name = "Alex"
     assert student.get_name == "Alex"
 
 def test_student_age_setter():
-    student = Student("Alice")
+    student = Student("Alice", 14, "12th")
     student.set_age = 14
     assert student.get_age == 14
 
 def test_student_grade_setter():
-    student = Student("Alice")
+    student = Student("Alice", 12, "10th")
     student.set_grade = "10th"
     assert student.get_grade == "10th"
 
 def test_invalid_name_setter():
-    student = Student("Alice")
+    student = Student("Alice", 15, "11th")
     student.set_name = 123
     assert student.get_name == "Alice"
 
 def test_invalid_age_setter():
-    student = Student("Alice")
+    student = Student("Alice", 13, "11th")
     student.set_age = "invalid"
     assert student.get_age == 13
 
 def test_invalid_grade_setter():
-    student = Student("Alice")
+    student = Student("Alice", 14, "12th")
     student.set_grade = "13th"
     assert student.get_grade == "12th"
+    # assert student.advance == "Alice has advanced to the 13th grade"
 
 # Write tests for Class methods
